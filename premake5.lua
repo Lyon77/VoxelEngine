b3fa02008ee716e773d04dac7e21074411d78c5f
Lyon77 <rjl7799@gmail.com>
2020-08-11 21:24:21 -0400

    Initial commit. Added premake file.

---
+++ premake5.lua
@@ -0,0 +1,148 @@
+workspace "Voxel"
+	architecture "x86_64"
+	startproject "Voxel"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+	flags
+	{
+		"MultiProcessorCompile"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- Include directories relative to the root folder (solution directory)
+IncludeDir = {}
+IncludeDir["GLFW"] = "Voxel/vendor/GLFW/include"
+IncludeDir["Glad"] = "Voxel/vendor/Glad/include"
+IncludeDir["ImGui"] = "Voxel/vendor/imgui"
+IncludeDir["glm"] = "Voxel/vendor/glm"
+IncludeDir["stb_image"] = "Voxel/vendor/stb_image"
+
+group "Dependencies"
+	include "Voxel/vendor/GLFW"
+	include "Voxel/vendor/Glad"
+	include "Voxel/vendor/imgui"
+group ""
+
+project "Voxel"
+	location "Voxel"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	--pchheader "precompile.h"
+	--pchsource "Voxel/src/precompile.cpp"
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/vendor/stb_image/**.h",
+		"%{prj.name}/vendor/stb_image/**.cpp",
+		"%{prj.name}/vendor/glm/glm/**.hpp",
+		"%{prj.name}/vendor/glm/glm/**.inl"
+	}
+
+	defines
+	{
+		"_CRT_SECURE_NO_WARNINGS",
+		"GLFW_INCLUDE_NONE"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/src",
+		"%{prj.name}/vendor/spdlog/include",
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.Glad}",
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.glm}",
+		"%{IncludeDir.stb_image}"
+	}
+
+	links
+	{
+		"GLFW",
+		"Glad",
+		"ImGui",
+		"opengl32.lib"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines
+		{
+		}
+
+	filter "configurations:Debug"
+		defines "MZ_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "MZ_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "MZ_DIST"
+		runtime "Release"
+		optimize "on"
+
+project "Sandbox"
+	location "SandBox"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Voxel/vendor/spdlog/include",
+		"Voxel/src",
+		"Voxel/vendor",
+		"%{IncludeDir.glm}"
+	}
+
+	links
+	{
+		"Voxel"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		defines "MZ_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "MZ_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "MZ_DIST"
+		runtime "Release"
+		optimize "on"
